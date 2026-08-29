9067a2f6daf107421c13ff44a9c42106931116d9
Trương Công Hải <110220797+trgcghai@users.noreply.github.com>
2026-08-29 20:27:42 +0700

    Add new SVG logos: logo-empty-white.svg and logo-white.svg
    - Introduced logo-empty-white.svg with a transparent background and a simple design.
    - Added logo-white.svg featuring a detailed design with white strokes on a transparent background.

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,50 +1,39 @@
-"use client";
-
 import { ModeToggler } from "@/components/common/mode-toggler";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-import ScrambleIn, {
-  ScrambleInHandle,
-} from "@/components/fancy/text/scramble-in";
-import { useRef } from "react";
 import { AnimationToggle } from "@/components/common/animation-toggle";
+import Image from "next/image";
 
 const Header = () => {
-  const ref = useRef<ScrambleInHandle>(null);
-
   return (
-    <div
-      className="container max-w-7xl mt-8 mx-auto border rounded-sm px-4 py-2 flex items-center justify-end md:justify-between bg-neutral-50/40 dark:bg-neutral-800/40"
-      onMouseEnter={() => ref.current?.start()}
-    >
+    <div className="container max-w-7xl mt-8 mx-auto border rounded-sm px-4 py-2 flex items-center justify-end md:justify-between bg-neutral-50/40 dark:bg-neutral-800/40">
       <div className="hidden md:block">
-        <Link href="/">
-          <ScrambleIn
-            text={"Truong Cong Hai"}
-            scrambleSpeed={40}
-            scrambledLetterCount={5}
-            autoStart={true}
-            ref={ref}
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src="/logo/svg/logo-empty-black.svg"
+            alt="Logo"
+            width={50}
+            height={50}
           />
         </Link>
       </div>
       <div className="flex items-center justify-between md:justify-end gap-2 flex-1">
         <div className="space-x-4">
-          <Button variant="ghost" size="sm">
-            <Link href="/" className="hover:text-primary">
+          <Button variant="ghost" size="default">
+            <Link href="/" className="hover:text-primary text-sm">
               Portfolio
             </Link>
           </Button>
 
-          <Button variant="ghost" size="sm">
-            <Link href="/blogs" className="hover:text-primary">
+          <Button variant="ghost" size="default">
+            <Link href="/blogs" className="hover:text-primary text-sm">
               Blogs
             </Link>
           </Button>
 
-          <Button variant="ghost" size="sm">
-            <Link href="/projects" className="hover:text-primary">
+          <Button variant="ghost" size="default">
+            <Link href="/projects" className="hover:text-primary text-sm">
               Projects
             </Link>
           </Button>
