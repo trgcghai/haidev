30ce38284d93277a3e51141b3cd31b2690c72df3
Trương Công Hải <110220797+trgcghai@users.noreply.github.com>
2026-09-11 15:04:14 +0700

    feat: add locale proxy middleware for internationalization support

---
+++ proxy.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+const locales = ["en", "vi"];
+
+function getLocale(request: NextRequest) {
+  const languages = new Negotiator({
+    headers: {
+      "accept-language":
+        request.headers.get("accept-language") || "en-US,en;q=0.5",
+    },
+  }).languages();
+
+  return match(languages, locales, "en");
+}
+
+export function proxy(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const { pathname } = request.nextUrl;
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+  );
+
+  if (pathnameHasLocale) return;
+
+  // Redirect if there is no locale
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+
+  // e.g. incoming request is /products
+  // The new URL is now /en/products
+  return NextResponse.redirect(request.nextUrl);
+}
+
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next)
+    "/((?!_next).*)",
+  ],
+};
